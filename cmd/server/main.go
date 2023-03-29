d896b83894667d2bc12a3ec72a880944fde67ecb
Bruna Cotrim <bruna_viotto@hotmail.com>
2023-03-29 06:35:31 -0300

    feat: cria o servidor da API e o POST de produto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/brunacotrim/api-product/configs"
+	"github.com/brunacotrim/api-product/internal/entity"
+	"github.com/brunacotrim/api-product/internal/infra/database"
+	"github.com/brunacotrim/api-product/internal/infra/database/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -12,4 +18,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(config)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
